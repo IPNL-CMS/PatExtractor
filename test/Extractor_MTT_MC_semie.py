93444b038757b00c70d4cd9ea93b1de6482a613b
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-02-06 16:31:10 +0100

    changed: enable met phi correction by default

--- test/Extractor_MTT_MC_semie.py
+++ test/Extractor_MTT_MC_semie.py
@@ -1,6 +1,6 @@
 from Extractor_MTT_common import *
 
-process = createExtractorProcess(True, False, useShiftCorrectedMET = False, globalTag = "START53_V7G")
+process = createExtractorProcess(True, False, useShiftCorrectedMET = True, globalTag = "START53_V7G")
 
 process.source.fileNames = cms.untracked.vstring(
    '/store/user/sperries/ZPrimeToTTJets_M1000GeV_W10GeV_TuneZ2star_8TeV-madgraph-tauola/Zprime_1000_Narrow_START53_V7A_04Dec12-v1/bd09b58f34b981e2c3ef3678b9b096ed/patTuple_2_1_P9c.root' 
