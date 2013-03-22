2b710fcafe762e370844038866f878ddac27ce4a
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-03-22 12:46:20 +0100

    [Neutrino] changed: if no solution if found for pz, use only the real part.

--- src/KinFit.cc
+++ src/KinFit.cc
@@ -648,81 +648,90 @@ double KinFit::PzNeutrino(const TLorentzVector& lept, TLorentzVector& neut, cons
     delta = 0;
   }
 #else
-  if (delta < 0)     // No solution, try to correct MET
-  {
-    double rat = neut.Py() / neut.Px();
-
-    double u = 4./(lept.E()*lept.E())*((lept.Px()+rat*lept.Py())*(lept.Px()+rat*lept.Py())/(1+rat*rat)
-        -(lept.E()*lept.E())+(lept.Pz()*lept.Pz()));
-
-    double v = 4./(lept.E()*lept.E())*(m_w*m_w-lept.M()*lept.M())
-      *(lept.Px()+rat*lept.Py())/sqrt(1+rat*rat);
-
-    double w = (m_w*m_w-lept.M()*lept.M())*(m_w*m_w-lept.M()*lept.M())/(lept.E()*lept.E());
-
-    double deltan = v * v - 4 * u * w;
-
-    if (deltan < 0)
-      return 0; // Hopeless, MET can't be corrected
-
-    double pt      = 0.;
-    double corfact = 0.;
-
-    if(u==0)
+  do {
+    if (delta < 0)     // No solution, try to correct MET
     {
-      pt = -w/v;
-      if (pt <= 0) return 0; // There is no way out...
+      if (eventCorrected)
+        *eventCorrected = true;
 
-      corfact = pt / neut.Pt();
-    }
-    else // Deltan>=0 and u!=0
-    {
-      double pt2 = (v-(sqrt(deltan)))/(2*u);
-      double pt1 = (v+(sqrt(deltan)))/(2*u);
+      double rat = neut.Py() / neut.Px();
 
-      // Pas de correction car negative
-      if(pt1<=0 && pt2<=0) return 0;
+      double u = 4./(lept.E()*lept.E())*((lept.Px()+rat*lept.Py())*(lept.Px()+rat*lept.Py())/(1+rat*rat)
+          -(lept.E()*lept.E())+(lept.Pz()*lept.Pz()));
 
-      if(pt1>0 && pt2<0) pt=pt1;
-      if(pt2>0 && pt1<0) pt=pt2;
-      if(pt1>0 && pt2>0)
-      {
-        (fabs(pt1-neut.Pt())<=fabs(pt2-neut.Pt()))
-          ? pt=pt1
-          : pt=pt2;     
+      double v = 4./(lept.E()*lept.E())*(m_w*m_w-lept.M()*lept.M())
+        *(lept.Px()+rat*lept.Py())/sqrt(1+rat*rat);
+
+      double w = (m_w*m_w-lept.M()*lept.M())*(m_w*m_w-lept.M()*lept.M())/(lept.E()*lept.E());
+
+      double deltan = v * v - 4 * u * w;
+
+      if (deltan < 0) {
+        delta = 0;
+        break;
       }
 
-      corfact = pt/neut.Pt();
+      double pt      = 0.;
+      double corfact = 0.;
+
+      if(u==0)
+      {
+        pt = -w/v;
+        if (pt <= 0) {
+          delta = 0;
+          break;
+        }
+
+        corfact = pt / neut.Pt();
+      }
+      else // Deltan>=0 and u!=0
+      {
+        double pt2 = (v-(sqrt(deltan)))/(2*u);
+        double pt1 = (v+(sqrt(deltan)))/(2*u);
+
+        if (pt1 <= 0 && pt2 <= 0) {
+          delta = 0;
+          break;
+        }
+
+        if(pt1>0 && pt2<0) pt=pt1;
+        if(pt2>0 && pt1<0) pt=pt2;
+        if(pt1>0 && pt2>0)
+        {
+          (fabs(pt1-neut.Pt())<=fabs(pt2-neut.Pt()))
+            ? pt=pt1
+            : pt=pt2;     
+        }
+
+        corfact = pt/neut.Pt();
+      }
+
+      // Now we have the correction factor
+
+      neut.SetPx(corfact*neut.Px());
+      neut.SetPy(corfact*neut.Py());
+
+      // Recompute the new parameters
+
+      x = (m_w*m_w-lept.M()*lept.M()+2.*(neut.Px()*lept.Px()+neut.Py()*lept.Py()))/(2*lept.E());
+      a = 1-(lept.Pz()*lept.Pz())/(lept.E()*lept.E());
+      b = -2.*(lept.Pz()/lept.E())*x;
+      c = neut.Px()*neut.Px()+neut.Py()*neut.Py()-x*x;
+
+  //         std::cout << "We have rescaled the MET " << lept->E() << " / " << corfact <<  " , now delta should be null:" << std::endl;
+  //         std::cout << "Previous delta: " << delta<< std::endl;
+
+
+      delta= b*b-4*a*c;
+
+      if (fabs(delta)<0.000001) delta=0.;
+
+  //    std::cout << "New delta     : " << delta << std::endl;
+
+      if (delta != 0)
+        delta = 0;
     }
-
-    // Now we have the correction factor
-
-    neut.SetPx(corfact*neut.Px());
-    neut.SetPy(corfact*neut.Py());
-
-    // Recompute the new parameters
-
-    x = (m_w*m_w-lept.M()*lept.M()+2.*(neut.Px()*lept.Px()+neut.Py()*lept.Py()))/(2*lept.E());
-    a = 1-(lept.Pz()*lept.Pz())/(lept.E()*lept.E());
-    b = -2.*(lept.Pz()/lept.E())*x;
-    c = neut.Px()*neut.Px()+neut.Py()*neut.Py()-x*x;
-
-//         std::cout << "We have rescaled the MET " << lept->E() << " / " << corfact <<  " , now delta should be null:" << std::endl;
-//         std::cout << "Previous delta: " << delta<< std::endl;
-
-
-    delta= b*b-4*a*c;
-
-    if (fabs(delta)<0.000001) delta=0.;
-
-//    std::cout << "New delta     : " << delta << std::endl;
-
-    if (delta != 0)
-      return 0; // This should not happen, but who knows...
-
-    if (eventCorrected)
-      *eventCorrected = true;
-  }
+  } while (false);
 #endif
 
 
