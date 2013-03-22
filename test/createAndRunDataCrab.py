02194f0af04127414751d491b639925d1eb5a620
Sébastien Brochet <s.brochet@ipnl.in2p3.fr>
2013-03-22 13:35:28 +0100

    updated global tags

--- test/createAndRunDataCrab.py
+++ test/createAndRunDataCrab.py
@@ -12,8 +12,8 @@ datasets = [
     ["/ElectronHad/sbrochet-ElectronHad_Run2012A-13Jul2012_22Nov12-v1-482cfd8beeb5bd50ce95db8c4b04846e/USER", "ElectronHad_Run2012A-13Jul2012", "FT_53_V6C_AN4"],
     ["/ElectronHad/sbrochet-ElectronHad_Run2012A-recover-06Aug2012_22Nov12-v1-a4e328caacc917c09c291e66055c015f/USER", "ElectronHad_Run2012A-recover-06Aug2012", "FT_53_V6C_AN4"],
     ["/SingleElectron/sbrochet-SingleElectron_Run2012B-TOPElePlusJets-13Jul2012_22Nov12-v1-482cfd8beeb5bd50ce95db8c4b04846e/USER", "SingleElectron_Run2012B-TOPElePlusJets-13Jul2012", "FT_53_V6C_AN4"],
-    ["/SingleElectron/sbrochet-SingleElectron_Run2012C-TOPElePlusJets-24Aug2012_22Nov12-v1-2648d2ac41c8a47bf1ce16deec221c74/USER", "SingleElectron_Run2012C-TOPElePlusJets-24Aug2012", "FT_53_V10_AN4"],
-    ["/SingleElectron/sbrochet-SingleElectron_Run2012C-TOPElePlusJets-PromptSkim_22Nov12-v1-a5e8ad198ec0ef3c5777633fcf11cc8c/USER", "SingleElectron_Run2012C-TOPElePlusJets-PromptSkim", "GR_P_V41_AN4"],
+    ["/SingleElectron/sbrochet-SingleElectron_Run2012C-TOPElePlusJets-24Aug2012_22Nov12-v1-2648d2ac41c8a47bf1ce16deec221c74/USER", "SingleElectron_Run2012C-TOPElePlusJets-24Aug2012", "FT53_V10A_AN4"],
+    ["/SingleElectron/sbrochet-SingleElectron_Run2012C-TOPElePlusJets-PromptSkim_22Nov12-v1-a5e8ad198ec0ef3c5777633fcf11cc8c/USER", "SingleElectron_Run2012C-TOPElePlusJets-PromptSkim", "GR_P_V42_AN4"],
     ["/SingleElectron/sbrochet-SingleElectron_Run2012C-EcalRecover_11Dec2012_08Jan13-v1-6731ea81b41c97e5dbdd3f3a8362a0ec/USER", "SingleElectron_Run2012C-EcalRecover-11Dec2012", "FT_P_V42C_AN4"],
     ["/SingleElectron/sbrochet-SingleElectron_Run2012D-TOPElePlusJets-PromptSkim_22Nov12-v1-ba18d001f77ddfaed5a189360146b128/USER", "SingleElectron_Run2012D-TOPElePlusJets-PromptSkim", "GR_P_V42_AN4"],
     ["/SingleElectron/sbrochet-SingleElectron_Run2012D-TOPElePlusJets-PromptSkim_03Jan13-v1-f93de0db34ee1c59360dcf3ada80214e/USER", "SingleElectron_Run2012D-TOPElePlusJets-PromptSkim_part2", "GR_P_V42_AN4"],
@@ -21,8 +21,8 @@ datasets = [
     ["/MuHad/sbrochet-MuHad_Run2012A-13Jul2012_22Nov12-v1-482cfd8beeb5bd50ce95db8c4b04846e/USER", "MuHad_Run2012A-13Jul2012", "FT_53_V6C_AN4"],
     ["/MuHad/sbrochet-MuHad_Run2012A-recover-06Aug2012_22Nov12-v1-a4e328caacc917c09c291e66055c015f/USER", "MuHad_Run2012A-recover-06Aug2012", "FT_53_V6C_AN4"],
     ["/SingleMu/sbrochet-SingleMu_Run2012B-TOPMuPlusJets-13Jul2012_22Nov12-v1-482cfd8beeb5bd50ce95db8c4b04846e/USER", "SingleMu_Run2012B-TOPMuPlusJets-13Jul2012", "FT_53_V6C_AN4"],
-    ["/SingleMu/sbrochet-SingleMu_Run2012C-TOPMuPlusJets-24Aug2012_22Nov12-v1-2648d2ac41c8a47bf1ce16deec221c74/USER", "SingleMu_Run2012C-TOPMuPlusJets-24Aug2012", "FT_53_V10_AN4"],
-    ["/SingleMu/sbrochet-SingleMu_Run2012C-TOPMuPlusJets-PromptSkim_22Nov12-v1-a5e8ad198ec0ef3c5777633fcf11cc8c/USER", "SingleMu_Run2012C-TOPMuPlusJets-PromptSkim", "GR_P_V41_AN4"],
+    ["/SingleMu/sbrochet-SingleMu_Run2012C-TOPMuPlusJets-24Aug2012_22Nov12-v1-2648d2ac41c8a47bf1ce16deec221c74/USER", "SingleMu_Run2012C-TOPMuPlusJets-24Aug2012", "FT53_V10A_AN4"],
+    ["/SingleMu/sbrochet-SingleMu_Run2012C-TOPMuPlusJets-PromptSkim_22Nov12-v1-a5e8ad198ec0ef3c5777633fcf11cc8c/USER", "SingleMu_Run2012C-TOPMuPlusJets-PromptSkim", "GR_P_V42_AN4"],
     ["/SingleMu/sbrochet-SingleMu_Run2012C-EcalRecover_11Dec2012_03Jan13-v1-6731ea81b41c97e5dbdd3f3a8362a0ec/USER", "SingleMu_Run2012C-EcalRecover-11Dec2012", "FT_P_V42C_AN4"],
     ["/SingleMu/sbrochet-SingleMu_Run2012D-TOPMuPlusJets-PromptSkim_22Nov12-v1-ba18d001f77ddfaed5a189360146b128/USER", "SingleMu_Run2012D-TOPMuPlusJets-PromptSkim", "GR_P_V42_AN4"],
     ["/SingleMu/sbrochet-SingleMu_Run2012D-TOPMuPlusJets-PromptSkim_03Jan13-v1-f93de0db34ee1c59360dcf3ada80214e/USER", "SingleMu_Run2012D-TOPMuPlusJets-PromptSkim_part2", "GR_P_V42_AN4"],
@@ -30,8 +30,8 @@ datasets = [
     ## ["/DoubleMu/chassera-DoubleMu_Run2012A-13Jul2012_27Nov12-v1-482cfd8beeb5bd50ce95db8c4b04846e/USER", "DoubleMu_Run2012A-13Jul2012", "FT_53_V6C_AN4"],
 ##     ["/DoubleMu/chassera-DoubleMu_Run2012A-recover-06Aug2012_27Nov12-v1-a4e328caacc917c09c291e66055c015f/USER", "DoubleMu_Run2012A-recover-06Aug2012", "FT_53_V6C_AN4"],
 ##     ["/DoubleMu/chassera-DoubleMu_Run2012B-13Jul2012_27Nov12-v1-482cfd8beeb5bd50ce95db8c4b04846e/USER", "DoubleMu_Run2012B-13Jul2012", "FT_53_V6C_AN4"],
-##     ["/DoubleMu/chassera-DoubleMu_Run2012C-24Aug2012_27Nov12-v1-2648d2ac41c8a47bf1ce16deec221c74/USER", "DoubleMu_Run2012C-13Jul2012", "FT_53_V10_AN4"],
-##     ["/DoubleMu/chassera-DoubleMu_Run2012C-PromptReco-v2_11Jan13-v1-a5e8ad198ec0ef3c5777633fcf11cc8c/USER", "DoubleMu_Run2012C-PromptReco", "GR_P_V41_AN4"],
+##     ["/DoubleMu/chassera-DoubleMu_Run2012C-24Aug2012_27Nov12-v1-2648d2ac41c8a47bf1ce16deec221c74/USER", "DoubleMu_Run2012C-13Jul2012", "FT53_V10A_AN4"],
+##     ["/DoubleMu/chassera-DoubleMu_Run2012C-PromptReco-v2_11Jan13-v1-a5e8ad198ec0ef3c5777633fcf11cc8c/USER", "DoubleMu_Run2012C-PromptReco", "GR_P_V42_AN4"],
 ##     ["/DoubleMu/chassera-DoubleMu_Run2012D-PromptReco-v1_11Jan13-v1-ba18d001f77ddfaed5a189360146b128/USER", "DoubleMu_Run2012D-PromptReco", "GR_P_V42_AN4"]
     ]
 
