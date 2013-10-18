IPNL-CMS/PatExtractor

commit d62e6a811d09342f2e8a2c9af1e05f8c06ab212d

Added Jpsi-to-mumu reconstruction and pairing

src/MCExtractor.cc
@@ -1,8 +1,9 @@
 #include "../interface/MCExtractor.h"
 
 
-MCExtractor::MCExtractor(const std::string& name, bool doTree)
+MCExtractor::MCExtractor(const std::string& name, bool doTree, bool doJpsi)
 {
+  _doJpsi = doJpsi; 
   // Set everything to 0
 
   m_OK = false;
@@ -31,12 +32,19 @@ MCExtractor::MCExtractor(const std::string& name, bool doTree)
     m_tree_MC->Branch("MC_vy",  &m_MC_vy,   "MC_vy[n_MCs]/F");  
     m_tree_MC->Branch("MC_vz",  &m_MC_vz,   "MC_vz[n_MCs]/F");
     m_tree_MC->Branch("MC_eta", &m_MC_eta,  "MC_eta[n_MCs]/F");  
-    m_tree_MC->Branch("MC_phi", &m_MC_phi,  "MC_phi[n_MCs]/F");  
+    m_tree_MC->Branch("MC_phi", &m_MC_phi,  "MC_phi[n_MCs]/F"); 
+    if (_doJpsi) {
+      m_tree_MC->Branch("MC_JPsiFromTop",  &m_MC_JPsiFromTop,  "m_MC_JPsiFromTop[n_MCs]/B");  
+      m_tree_MC->Branch("MC_JPsiFromAntiTop",  &m_MC_JPsiFromAntiTop,  "m_MC_JPsiFromAntiTop[n_MCs]/B");  
+      m_tree_MC->Branch("MC_LeptonFromTop",  &m_MC_LeptonFromTop,  "m_MC_LeptonFromTop[n_MCs]/B");  
+      m_tree_MC->Branch("MC_LeptonFromAntiTop",  &m_MC_LeptonFromAntiTop,  "m_MC_LeptonFromAntiTop[n_MCs]/B");
+    }
   }
 }
 
-MCExtractor::MCExtractor(const std::string& name, TFile *a_file)
+MCExtractor::MCExtractor(const std::string& name, TFile *a_file, bool doJpsi)
 {
+  _doJpsi = doJpsi; 
   std::cout << "MCExtractor object is retrieved" << std::endl;
 
   // Tree definition
@@ -86,6 +94,16 @@ MCExtractor::MCExtractor(const std::string& name, TFile *a_file)
     m_tree_MC->SetBranchAddress("MC_eta", &m_MC_eta);
   if (m_tree_MC->FindBranch("MC_phi")) 
     m_tree_MC->SetBranchAddress("MC_phi", &m_MC_phi);
+  if (_doJpsi) {
+    if (m_tree_MC->FindBranch("MC_JPsiFromTop")) 
+      m_tree_MC->SetBranchAddress("MC_JPsiFromTop", &m_MC_JPsiFromTop);
+    if (m_tree_MC->FindBranch("MC_JPsiFromAntiTop")) 
+      m_tree_MC->SetBranchAddress("MC_JPsiFromAntiTop", &m_MC_JPsiFromAntiTop);
+    if (m_tree_MC->FindBranch("MC_LeptonFromTop")) 
+      m_tree_MC->SetBranchAddress("MC_LeptonFromTop", &m_MC_LeptonFromTop);
+    if (m_tree_MC->FindBranch("MC_LeptonFromAntiTop")) 
+      m_tree_MC->SetBranchAddress("MC_LeptonFromAntiTop", &m_MC_LeptonFromAntiTop);
+  }
 }
 
 
@@ -118,6 +136,9 @@ void MCExtractor::writeInfo(const edm::Event& event, const edm::EventSetup& iSet
 
   int ipart = 0;
 
+  const reco::GenParticle* mothertmp;
+  const reco::GenParticle* motherleptontmp;
+
   for(int i=0; i < m_n_MCs; ++i) 
   {
     const reco::Candidate & p = (*genParticles)[i];
@@ -130,7 +151,7 @@ void MCExtractor::writeInfo(const edm::Event& event, const edm::EventSetup& iSet
     int iMo1 = -1;
     int iMo2 = -1;
 
-    if (st == 3)
+    if (st == 3 || ( _doJpsi && id == 443 && p.numberOfDaughters() == 2 && fabs(p.daughter(0)->pdgId()) ==  13 && fabs(p.daughter(1)->pdgId()) ==  13))
     {
 
       // MC@NLO use different status code
@@ -197,7 +218,50 @@ void MCExtractor::writeInfo(const edm::Event& event, const edm::EventSetup& iSet
       m_MC_vz[ipart]         = p.vz();
       m_MC_eta[ipart]        = p.eta();
       m_MC_phi[ipart]        = p.phi();
-      new((*m_MC_lorentzvector)[ipart]) TLorentzVector(p.px(),p.py(),p.pz(),p.energy());
+      new((*m_MC_lorentzvector)[i]) TLorentzVector(p.px(),p.py(),p.pz(),p.energy());
+
+      if (_doJpsi && id==443) {
+        mothertmp = &(*genParticles)[i];
+        for (int ifrag=0; ifrag<10; ifrag++) {
+          if (mothertmp->mother() == 0) break;
+          mothertmp = (reco::GenParticle*) mothertmp->mother();
+          if (fabs(mothertmp->pdgId())==92 || fabs(mothertmp->pdgId())==91) break;
+        }
+        for (int imb=0; imb<100; imb++) {
+          if (mothertmp->mother(imb) != 0 && fabs(mothertmp->mother(imb)->pdgId())==5) {
+            mothertmp = (reco::GenParticle*) mothertmp->mother(imb);
+            break;
+          } 
+        }
+        if (fabs(mothertmp->pdgId()) == 5) {
+          for (int ib=0; ib<10; ib++) {
+            if (mothertmp->mother() !=0 && (fabs(mothertmp->mother()->pdgId())==5 || fabs(mothertmp->mother()->pdgId())==21)) mothertmp = (reco::GenParticle*) mothertmp->mother();
+            else break;
+          }
+          if(mothertmp->mother() !=0) mothertmp = (reco::GenParticle*) mothertmp->mother();
+          if (mothertmp->pdgId()==6)  m_MC_JPsiFromTop[ipart] = true;
+          if (mothertmp->pdgId()==-6) m_MC_JPsiFromAntiTop[ipart] = true;
+        }	
+        //std::cout << "JPsiFromTop = " << m_MC_JPsiFromTop[ipart] << "JPsiFromAntiTop = " << m_MC_JPsiFromAntiTop[ipart] << std::endl;
+      }	
+      if (_doJpsi && (fabs(id)==11 || fabs(id) ==13)) {
+        motherleptontmp = &(*genParticles)[i];
+        for (int i=0; i<10; i++) {
+          if (motherleptontmp->mother() == 0) break;
+          motherleptontmp = (reco::GenParticle*) motherleptontmp->mother();
+          if (motherleptontmp->pdgId()==6) {
+            m_MC_LeptonFromTop[ipart] = true;
+            break;
+          }
+          if (motherleptontmp->pdgId()==-6) {
+            m_MC_LeptonFromAntiTop[ipart] = true;
+            break;
+          }
+        }
+        //std::cout << "LeptonFromTop = " << m_MC_LeptonFromTop[ipart] << "LeptonFromAntiTop = " << m_MC_LeptonFromAntiTop[ipart] << std::endl;
+        if (m_MC_LeptonFromTop[ipart]==false && m_MC_LeptonFromAntiTop[ipart]==false) std::cout << "cas suspect" << std::endl;
+      }
+
 
       if (n_mot==0) m_MC_generation[ipart] = 0;
 
@@ -209,9 +273,7 @@ void MCExtractor::writeInfo(const edm::Event& event, const edm::EventSetup& iSet
   m_n_MCs = ipart;
 
   for(int i=1; i<6; ++i) 
-  {    
     MCExtractor::constructGeneration(i, m_n_MCs);
-  }
 
   fillTree();
 }
@@ -248,6 +310,12 @@ void MCExtractor::reset()
     m_MC_vz[i] = 0.;
     m_MC_eta[i] = 0.;
     m_MC_phi[i] = 0.; 
+    if (_doJpsi) {
+      m_MC_JPsiFromTop[i] = false;
+      m_MC_JPsiFromAntiTop[i] = false;
+      m_MC_LeptonFromTop[i] = false;
+      m_MC_LeptonFromAntiTop[i] = false;
+    }
   }
   m_MC_lorentzvector->Clear();
 }

BuildFile.xml
@@ -44,6 +44,10 @@
 <use   name="EGamma/EGammaAnalysisTools" />
 <use   name="RecoBTag/PerformanceDB" />
 <use   name="RecoBTag/Records" />
+<use   name="PhysicsTools/CandUtils" />
+<use   name="RecoVertex/KinematicFit"/>
+<use   name="RecoVertex/KinematicFitPrimitives"/>
+<use   name="RecoVertex/KalmanVertexFit"/>
 
 <lib   name="MathMore"/>
 

interface/MCExtractor.h
@@ -30,8 +30,8 @@ class MCExtractor: public SuperBaseExtractor
 
  public:
 
-  MCExtractor(const std::string& name, bool doTree);
-  MCExtractor(const std::string& name, TFile *a_file);
+  MCExtractor(const std::string& name, bool doTree, bool doJpsi = false);
+  MCExtractor(const std::string& name, TFile *a_file, bool doJpsi = false);
   virtual ~MCExtractor();
 
   virtual void writeInfo(const edm::Event& event, const edm::EventSetup& iSetup, MCExtractor* mcExtractor);
@@ -57,7 +57,6 @@ class MCExtractor: public SuperBaseExtractor
 
   int getPatIndex(int index) const { return m_MC_index[index]; }
 
-
  private:
   
   TTree* m_tree_MC;
@@ -81,6 +80,12 @@ class MCExtractor: public SuperBaseExtractor
   float	m_MC_vz[m_MCs_MAX];
   float	m_MC_eta[m_MCs_MAX];
   float	m_MC_phi[m_MCs_MAX];
+  
+  bool _doJpsi;
+  bool m_MC_JPsiFromTop[m_MCs_MAX];
+  bool m_MC_JPsiFromAntiTop[m_MCs_MAX];
+  bool m_MC_LeptonFromTop[m_MCs_MAX];
+  bool m_MC_LeptonFromAntiTop[m_MCs_MAX];
 
   void constructGeneration(int gene, int npart);
 };

interface/PFpartExtractor.h
@@ -38,8 +38,11 @@ class PFpartExtractor: public BaseExtractor<reco::PFCandidate>
     PFpartExtractor(const std::string& name, const edm::InputTag& tag, bool doTree);
     PFpartExtractor(const std::string& name, TFile* a_file);
     virtual ~PFpartExtractor();
-
+    
+    // Dummy pure virtual function in BaseExtractor
     void writeInfo(const edm::Event& event, const edm::EventSetup& iSetup, const reco::PFCandidate& part, int index);
+    // Function doing the real job
+    void writeInfo(const edm::Event& event, const edm::EventSetup& iSetup, MCExtractor* mcExtractor);
 
     virtual const reco::Candidate* getGenParticle(const reco::PFCandidate& part) {
       return NULL;
@@ -82,14 +85,43 @@ class PFpartExtractor: public BaseExtractor<reco::PFCandidate>
     static const int 	m_pfpart_MAX  = 100;
 
     int         m_pf_size;
-
     TClonesArray* m_pf_lorentzvector;
     float	m_pf_vx[m_pfpart_MAX];
     float	m_pf_vy[m_pfpart_MAX];
     float	m_pf_vz[m_pfpart_MAX];
     int	        m_pf_charge[m_pfpart_MAX];
     int         m_pf_pdgid[m_pfpart_MAX];
-
+    int         m_pf_trkLayer[m_pfpart_MAX];
+    int         m_pf_pixLayer[m_pfpart_MAX];
+    float       m_pf_trknormChi2[m_pfpart_MAX];
+    int         m_pf_numberOfChambers[m_pfpart_MAX];
+    int         m_pf_numberOfMatchedStations[m_pfpart_MAX];
+    bool        m_pf_isGlobalMuon[m_pfpart_MAX];
+    bool        m_pf_isTrackerMuon[m_pfpart_MAX];
+    bool        m_pf_isStandAloneMuon[m_pfpart_MAX];
+    bool        m_pf_isCaloMuon[m_pfpart_MAX];
+    bool        m_pf_isPFMuon[m_pfpart_MAX];
+    bool        m_pf_isRPCMuon[m_pfpart_MAX];
+    
+    static const int 	m_jpsi_MAX  = 10;
+    
+    int           m_jpsi_size;
+    int           m_jpsi_indpf1[m_jpsi_MAX];
+    int           m_jpsi_indpf2[m_jpsi_MAX];
+    TClonesArray* m_jpsimu_lorentzvector;
+    TClonesArray* m_jpsiraw_lorentzvector;
+    TClonesArray* m_jpsiraw_mu1_lorentzvector;
+    TClonesArray* m_jpsiraw_mu2_lorentzvector;
+    float         m_jpsiraw_vx[m_jpsi_MAX];
+    float         m_jpsiraw_vy[m_jpsi_MAX];
+    float         m_jpsiraw_vz[m_jpsi_MAX];
+    bool          m_jpsiraw_vtxvalid[m_jpsi_MAX];
+    float         m_jpsiraw_vtxchi2[m_jpsi_MAX];
+    float         m_jpsiraw_ndf[m_jpsi_MAX];
+    float         m_jpsiraw_L3D[m_jpsi_MAX];
+    float         m_jpsiraw_sigmaL3D[m_jpsi_MAX];
+    float         m_jpsiraw_L3DoverSigmaL3D[m_jpsi_MAX];
+    
 };
 
 #endif 

interface/PatExtractor.h
@@ -78,6 +78,7 @@ class PatExtractor : public edm::EDAnalyzer {
   bool do_fill_;
   bool do_HLT_;
   bool do_MC_;
+  bool do_MCjpsi_;
   bool do_Photon_;
   bool do_Electron_;
 

python/PAT_extractor_cfi.py
@@ -58,7 +58,8 @@ PATextraction = cms.EDAnalyzer("PatExtractor",
    # Add MC information
    doMC          = cms.untracked.bool(False),
    MC_tag        = cms.InputTag( "" ),
-                               
+   doMCjpsi      = cms.untracked.bool(False),
+                             
    # Add Photon information
    doPhoton      = cms.untracked.bool(False),
    photon_tag    = cms.InputTag( "selectedPatPhotons" ),

src/PatExtractor.cc
@@ -8,6 +8,7 @@ PatExtractor::PatExtractor(const edm::ParameterSet& config) :
   do_fill_       (config.getUntrackedParameter<bool>("fillTree", true)),
   do_HLT_        (config.getUntrackedParameter<bool>("doHLT", false)),
   do_MC_         (config.getUntrackedParameter<bool>("doMC", false)),
+  do_MCjpsi_     (config.getUntrackedParameter<bool>("doMCjpsi", false)),
   do_Photon_     (config.getUntrackedParameter<bool>("doPhoton", false)),
   do_Electron_   (config.getUntrackedParameter<bool>("doElectron", false)),
 
@@ -238,7 +239,7 @@ void PatExtractor::initialize(const edm::ParameterSet& config)
   addExtractor("event", new EventExtractor("event"));
 
   if (do_MC_)
-    addExtractor("MC", new MCExtractor("MC", do_MC_));
+    addExtractor("MC", new MCExtractor("MC", do_MC_, do_MCjpsi_));
 
   if (do_HLT_)
     addExtractor("HLT", new HLTExtractor("HLT", do_HLT_, config));

commit 75bec7b44c57edbd06f19745e6af5d9dc65f1413

fixed regression from d62e6a811d09342f2e8a2c9af1e05f8c06ab212d

src/MCExtractor.cc
@@ -218,7 +218,7 @@ void MCExtractor::writeInfo(const edm::Event& event, const edm::EventSetup& iSet
       m_MC_vz[ipart]         = p.vz();
       m_MC_eta[ipart]        = p.eta();
       m_MC_phi[ipart]        = p.phi();
-      new((*m_MC_lorentzvector)[i]) TLorentzVector(p.px(),p.py(),p.pz(),p.energy());
+      new((*m_MC_lorentzvector)[ipart]) TLorentzVector(p.px(),p.py(),p.pz(),p.energy());
 
       if (_doJpsi && id==443) {
         mothertmp = &(*genParticles)[i];